8fccfa200fe6b0d38a5a5823965e813e0a5ca142
Sawyer <sawyer.miller12@gmail.com>
2022-02-22 17:42:46 -0600

    Attack Preview UI WIP

---
+++ Assets/Scripts/AttackPreviewUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AttackPreviewUI : MonoBehaviour
+{
+
+    public TMP_Text attackUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideAttackUI();
+    }
+
+    public void ShowAttackUI(Unit unit)
+    {
+        UpdateAttackUI(unit);
+
+        attackUI.gameObject.SetActive(true);
+    }
+
+    public void UpdateAttackUI(Unit unit)
+    {
+        attackUI.text = "" + unit.unitName + "\n" + (BattleManager.GetCurrentUnit().accuracy * 100) + "\n" + BattleManager.GetCurrentUnit().minDamage + "-" + BattleManager.GetCurrentUnit().maxDamage;
+    }
+
+    public void HideAttackUI()
+    {
+        attackUI.gameObject.SetActive(false);
+    }
+}
