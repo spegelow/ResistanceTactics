984abccc634ac308c79986c5639f1ae5f09805b9
Scott <svpegelow@gmail.com>
2022-03-01 10:37:26 -0600

    Improved Camera
    - Added additional camera movement options (WASD move and zoom)
    - Added rotation to unit health info
    - Minor camera tweaks to make movement and rotation better

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float rotationSpeed;
+    public float moveSpeed;
+    public float zoomSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -26,5 +28,34 @@ public class CameraController : MonoBehaviour
         {
             this.transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
         }
+
+        Vector3 moveDirection = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            moveDirection.z += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDirection.z += -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveDirection.x += -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveDirection.x += 1;
+        }
+
+        moveDirection = Vector3.ProjectOnPlane(moveDirection, Vector3.up);
+        moveDirection = Quaternion.AngleAxis(this.transform.rotation.eulerAngles.y, Vector3.up) * moveDirection;
+
+        //Move the camera relative to its facing and the xz-plane
+        this.transform.position += moveDirection.normalized * Time.deltaTime * moveSpeed;
+
+
+        float zoomFactor = Input.mouseScrollDelta.y * zoomSpeed;
+        Camera.main.orthographicSize += zoomFactor;
     }
 }
